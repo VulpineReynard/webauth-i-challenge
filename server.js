8a195cf63c0a11bf349377e20f6d5d3263e5544f
VulpineReynard <milare8@gmail.com>
2019-09-16 18:20:36 -0400

    server running and responding

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
+const userRouter = require('./users/users-router.js');
+const Users = require('./users/users-model.js');
+const restricted = require('./auth/restricted.js');
 
 const server = express();
 
@@ -9,10 +12,25 @@ const server = express();
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+server.use('/api/users', userRouter);
 
 server.get('/', (req, res) => {
   // get all species from the database
   res.status(200).send({ message: "Good to go." })
 });
+
+server.post('/api/register', (req, res) => {
+  let { username, password } = req.body;
+
+  const hash = bcrypt.hashSync(password, 8);
+
+  Users.add({ username, password: hash })
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+})
 
 module.exports = server;
--- users/users-router.js
+++ users/users-router.js
@@ -1,1 +1,7 @@
-const restricted = require('./auth/restricted.js');
+const express = require('express');
+const restricted = require('../auth/restricted.js');
+
+const router = express.Router();
+
+
+module.exports = router;
